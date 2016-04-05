295a2cb63b65a8c7e843a33d019437125f49375d
MIBUK <m.reda@inthezone.co.uk>
2016-04-06 00:05:59 +0200

    Updated object properties

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,10 +17,11 @@ var bio = {
        "location": "Stockholm, Sweden"
       }
      ],
-     "welcome": "Welcome to my online resume. It will summarise my skills, history and contact info.",
+     "welcomeMessage": "Welcome to my online resume. It will summarise my skills, history and contact info.",
      "skills": [
      "HTML", "CSS", "JavaScript", "Workflow Tools", "Variable Data", "Networks", "Crossmedia Tools"
-     ]
+     ],
+     "biopic": "http://www.hotmodelsactress.com/actors/images/Alec_Baldwin.jpg"
 };
 
 // JSON containing work information
@@ -81,28 +82,29 @@ var education = {
      { 
       "name": "West Herts College",
       "location": "Watford, UK",
-      "certification": "HNC",
-      "subject": ["Software Engineering"],
+      "degree": "HNC",
+      "majors": ["Software Engineering"],
       "dates": "1992-1994",
       "url": "https://www.westherts.ac.uk/"
      },
      { 
       "name": "Leggatts School",
       "location": "Watford, UK",
-      "certification": "O'Levels",
-      "subject": ["English", " Maths"],
+      "degree": "O'Levels",
+      "majors": ["English", " Maths"],
       "dates": "1981-1986",
       "url": "https://www.facebook.com/pages/Leggatts-school/104261299619366?rf=108533885843749"
      }
     ],
-     "onlinecourses": [
+     "onlineCourses": [
      {
       "title": "Udacity NanoDegree",
       "school": "Udacity",
-      "dates": "2016",
+      "date": "2016",
       "url": "http://www.udacity.com"
      }
     ]
+  
 };
 
 // A function to display biography information via JQuery selectors to append data to 
@@ -110,7 +112,7 @@ var education = {
 // Using an IF statement to append the skills from the skills array and a FOR loop to
 // display contact information from the contacts array
 
-function displayBio() {
+bio.display = function() {
     var rolebio = HTMLheaderRole.replace
     ("%data%", bio.role);
     $("#header").prepend(rolebio);
@@ -118,10 +120,10 @@ function displayBio() {
     ("%data%", bio.name);
     $("#header").prepend(namebio);
     var imagebio = HTMLbioPic.replace
-    ("%data%", "http://www.hotmodelsactress.com/actors/images/Alec_Baldwin.jpg");
+    ("%data%", bio.biopic);
     $("#header").append(imagebio);
     var welcomebio = HTMLwelcomeMsg.replace
-    ("%data%", bio.welcome);
+    ("%data%", bio.welcomeMessage);
     $("#header").append(welcomebio);
 
     if (bio.skills.length > 0) {
@@ -176,13 +178,13 @@ function displayBio() {
 
 // Calls the displayBio function to display the data in the bio object on the web page
 
-displayBio();
+bio.display();
 
 // A function to display work information via JQuery selectors to append data to 
 // HTML using variables declared in the helper.js file.
 // Using FOR loop to display work information from the work object
 
-function displayWork() {
+work.display = function() {
     for (var indexCount = 0; indexCount < work.jobs.length; indexCount++) {
     	$("#workExperience").append(HTMLworkStart);
     	var formattedWork = HTMLworkEmployer.replace
@@ -205,7 +207,7 @@ function displayWork() {
 
 // Calls the displayWork function to display the data in the work object on the web page
 
-displayWork();
+work.display();
 
 // A function to display projects information via JQuery selectors to append data to 
 // HTML using variables declared in the helper.js file.
@@ -244,13 +246,13 @@ projects.display();
 // Using 2 FOR in loops, the first one to select data in the schools array and the second 
 // FOR loop to display the data within the onlinecourses array
 
-function displayEducation() {
+education.display = function () {
     for (var indexCount = 0; indexCount < education.schools.length; indexCount++) {
         $("#education").append(HTMLschoolStart);
         var formattedname = HTMLschoolName.replace
         ("%data%", education.schools[indexCount].name);
         var formatteddegree = HTMLschoolDegree.replace
-        ("%data%", education.schools[indexCount].certification);
+        ("%data%", education.schools[indexCount].degree);
         var formattednamedegree = formattedname + formatteddegree;
         $(".education-entry:last").append(formattednamedegree);
         var formattedLocation = HTMLschoolLocation.replace
@@ -260,34 +262,34 @@ function displayEducation() {
         ("%data%", education.schools[indexCount].dates);
         $(".education-entry:last").append(formattedDates);
         var formattedMajor = HTMLschoolMajor.replace
-        ("%data%", education.schools[indexCount].subject);
+        ("%data%", education.schools[indexCount].majors);
         $(".education-entry:last").append(formattedMajor);
         var formattedURL = HTMLschoolURL.replace
         ("%data%", education.schools[indexCount].url);
         $(".education-entry:last").append(formattedURL);
         }
 
-        for (var indexCount = 0; indexCount < education.onlinecourses.length; indexCount++) {
+        for (var indexCount = 0; indexCount < education.onlineCourses.length; indexCount++) {
             $("#education").append(HTMLonlineClasses);
             $("#education").append(HTMLschoolStart);
             var formattedtitle = HTMLonlineTitle.replace
-            ("%data%", education.onlinecourses[indexCount].title);
+            ("%data%", education.onlineCourses[indexCount].title);
             var formattedschool = HTMLonlineSchool.replace
-            ("%data%", education.onlinecourses[indexCount].school);
+            ("%data%", education.onlineCourses[indexCount].school);
             var formattedtitleschool = formattedtitle + formattedschool;
             $(".education-entry:last").append(formattedtitleschool);
             var formatteddate = HTMLonlineDates.replace
-            ("%data%", education.onlinecourses[indexCount].dates);
+            ("%data%", education.onlineCourses[indexCount].date);
             $(".education-entry:last").append(formatteddate);
             var formattedurl = HTMLonlineURL.replace
-            ("%data%", education.onlinecourses[indexCount].url);
+            ("%data%", education.onlineCourses[indexCount].url);
             $(".education-entry:last").append(formattedurl);
             }
 }
 
 // Calls the displayEducation function to display the data in the education object on the web page
 
-displayEducation();
+education.display();
 
 // A JQUery selector to append the Google Map to the web page, which dsiplays the locations from the
 // bio, work and education
